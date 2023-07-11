604b2639fcc7c7c47e109bea2b48b001180eb3d3
looko <lookoutldz@outlook.com>
2023-07-11 13:33:35 +0800

    index页面访问改为非必需登录

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: '/index',
       name: 'index',
-      meta: { requireAuth: true },
+      meta: { requireAuth: false },
       component: () => import('../views/indexView.vue')
     },
     {
